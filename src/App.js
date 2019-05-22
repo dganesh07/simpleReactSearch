16531ca8d17b9470879111f8a7497080c52403a4
Damini Ganesh <damini.ganesh@covetool.com>
2019-05-22 13:04:01 -0400

    Initial Setup + dummy data

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Json from "./db.json";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h3>Simple React Search</h3>
+      <div>
+        <input type="text" value="" onChange="" placeholder="type name here" />
+        <ul>
+          {Json.map(l => {
+            return (
+              <li>
+                {l.name} <a href="#">{l.email}</a>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
     </div>
   );
 }
